f0dac536f3a1511c816816c40af8fddd29974894
“jdocampom” <jdocampom@unal.edu.co>
2020-10-31 10:48:35 -0500

    Initial Commit

---
+++ Destini/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Juan Diego Ocampo on 31/10/2020.
+//  Copyright © 2019 Juan Diego Ocampo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
---
+++ Destini/Model/Story.swift
@@ -0,0 +1,9 @@
+//
+//  Sotry.swift
+//  Destini
+//
+//  Created by Juan Diego Ocampo on 31/10/2020.
+//  Copyright © 2019 Juan Diego Ocampo. All rights reserved.
+//
+
+import Foundation
